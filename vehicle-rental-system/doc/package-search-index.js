744f123ec19fa3787e35a3871092282eb3dc0eb2
shahddaas622-web <shahddaas622@gmail.com>
2026-07-31 21:51:06 +0300

    Add generated Javadoc HTML documentation

---
+++ vehicle-rental-system/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"com.rental.domain.model"},{"l":"com.rental.exception"},{"l":"com.rental.persistence"},{"l":"com.rental.service"}];updateSearchResults();
